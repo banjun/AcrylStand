f1902b81586e653e858a8958820e92d7380b828d
banjun <banjun@gmail.com>
2024-03-21 20:31:45 +0900

    Initial Commit

---
+++ AcrylStand/AcrylStandApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcrylStandApp.swift
+//  AcrylStand
+//
+//  Created by BAN Jun on R 6/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AcrylStandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
