4e4265ff1c4fc049f84d0149c57fa423da6fcf8a
banjun <banjun@gmail.com>
2024-12-07 19:45:23 +0900

    use banjun/RCPMaterialParameters

--- AcrylStand/Acryl/Package.swift
+++ AcrylStand/Acryl/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Acryl",
     platforms: [
-        .visionOS(.v1),
+        .visionOS(.v2),
         .macOS(.v15),
         .iOS(.v18)
     ],
@@ -17,14 +17,13 @@ let package = Package(
             targets: ["Acryl"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/banjun/RCPMaterialParameters", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Acryl",
-            dependencies: []),
+            dependencies: ["RCPMaterialParameters"]),
     ]
 )
